e9cd08c3e6b3ba09158fd979ff30c31dfdd9eb8a
KCH317 <rnjscksgh94@gmail.com>
2022-03-20 23:57:24 +0900

    4-물마시기앱
    - UI마무리

---
+++ Drink/Drink/AddAlertViewController.swift
@@ -0,0 +1,28 @@
+//
+//  addAlertViewController.swift
+//  Drink
+//
+//  Created by 권찬호 on 2022/03/20.
+//
+
+import UIKit
+
+class AddAlertViewController: UIViewController {
+    
+    var pickedDate: ((_ date: Date) -> Void)?
+     
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func dismissButtonTapped(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        pickedDate?(datePicker.date)
+        self.dismiss(animated: true, completion: nil )
+    }
+}
